8544b81209765e58119182ec1ad3ad8ef7f4db3b
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-06-05 12:10:02 -0600

    Finished search bar functionality with API call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,52 @@
+// API Key
 const stockKey = `&apikey=BF997UXSJ2Q4JW3Y`;
-// Stock Symbol searched
+// Store user's searched stock symbols value
 var searchSymbol = $(`#searchStock`).val();
-// Stock Data API URL
-const dataAPI = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=` + searchSymbol + stockKey;
+
 
 // Lets user click search button with ENTER key
 // When you press ENTER key within the stock search bar
 $(`#searchStock`).keypress(function(event) {
   // Only if user pressed the enter key
   if (event.keyCode === 13) { 
-      // Prevent user text from resetting
-  event.preventDefault();
       // Click search button
   $(`#searchBtn`).click(); 
   } 
 });
 
 // On search button click
-$(`#searchBtn`).on(`click`, function() {
-  // Display stock data
-  callStock();
+$(`#searchBtn`).on(`click`, function(event) {
+  event.preventDefault();
+  // Get value stored in search box from user input
+  searchSymbol = $(`#searchStock`).val();
 
+  // Display stock data
+  // Stock Data API URL
+const dataAPI = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=` + searchSymbol + stockKey;
+
+  // Call Stock API
+$.ajax({
+  url: dataAPI,
+  method: `GET`
+}).then(function(response){
+  // All data API returned 
+  console.log(response);
+
+  // Call stockData function with JSON response
+  stockData(response);
 });
 
-// Call Stock API
-function callStock() {
-    $.ajax({
-    url: dataAPI,
-    method: `GET`
-  }).then(function(response){
-    // All data API returned 
-    console.log(response);
+ // Clear the value of the search box
+ searchSymbol = $(`#searchStock`).val(``);
+});
 
-    stockData(response);
-  })
-};
+
 
 function stockData(response) {
-    //   Clear the card
-    $(`#data`).val(``);
-    //   Display new stock data
-    $(`#data`).text(JSON.stringify(response));
+  //   Clear the card
+  $(`#data`).val(``);
+  //   Display new stock data
+  $(`#data`).text(JSON.stringify(response));
 };
 
 //   Call functions
